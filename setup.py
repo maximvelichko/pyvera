a94b5655a6f58aa8cfff85c316e355e658d69097
pavoni <mail@gregdowling.com>
2016-01-15 10:32:03 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyvera',
-      version='0.2.3',
+      version='0.2.4',
       description='Python API for talking to Vera Z-Wave controllers',
       url='https://github.com/pavoni/pyvera',
       author='James Cole, Greg Dowling',
