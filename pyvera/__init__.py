3a98a5000254d729d80bddf2ab9321a3eb5fb517
Greg Dowling <pavoni@users.noreply.github.com>
2020-11-07 12:29:01 +0000

    Fix merge error.

--- pyvera/__init__.py
+++ pyvera/__init__.py
@@ -816,7 +816,7 @@ class VeraSwitch(VeraDevice):
 class VeraDimmer(VeraSwitch):
     """Class to add dimmer functionality."""
 
-    def is_switched_on(self, refresh=False):
+    def def is_switched_on(self, refresh: bool = False) -> bool:
         """Get dimmer state.
 
         Refresh data from Vera if refresh is True,
